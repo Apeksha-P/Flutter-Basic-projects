08cfc0cdce99dab341bd5a92b48056cf637ef214
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-05-12 11:49:22 +0530

    Content Update

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -8,7 +8,40 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello Apeksha"),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Test App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Hello Apeksha",style: TextStyle(
+              color: Colors.redAccent,
+              fontSize: 50,
+              backgroundColor: Colors.blueGrey,
+            ),),
+            Text("You have pushed button this meny times: "),
+            Text("0", style: TextStyle(
+              fontSize: 25,
+              color: Colors.pink,
+              fontWeight: FontWeight.bold,
+            ),),
+          ],
+        ),
+      ),
     );
   }
 }

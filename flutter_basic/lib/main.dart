6fdc7791faeb35cf1ae13eaf0fad477106065453
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-05-12 12:31:46 +0530

    Content update

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -13,16 +13,33 @@ class MyApp extends StatelessWidget{
     );
   }
 }
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int value =0;
+  void increement(){
+    setState(() {
+      value++;
+      print(value);
+    });
+    
+  }
+ @override
   Widget build(BuildContext context) {
+  
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.blueAccent,
-        title: Text("Test App"),
+        title: Text("Test App", style: TextStyle(
+          fontSize: 30,
+          color: Colors.white,
+        ),),
       ),
       body: Center(
         child: Column(
@@ -34,7 +51,7 @@ class HomePage extends StatelessWidget {
               backgroundColor: Colors.blueGrey,
             ),),
             Text("You have pushed button this meny times: "),
-            Text("0", style: TextStyle(
+            Text("$value", style: TextStyle(
               fontSize: 25,
               color: Colors.pink,
               fontWeight: FontWeight.bold,
@@ -42,6 +59,56 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: increement
+      ),
     );
   }
 }
+// class HomePage extends StatelessWidget {
+//   const HomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     int value =0;
+//   void increement(){
+//     value++;
+//     print(value);
+//   }
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         backgroundColor: Colors.blueAccent,
+//         title: Text("Test App", style: TextStyle(
+//           fontSize: 30,
+//           color: Colors.white,
+//         ),),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text("Hello Apeksha",style: TextStyle(
+//               color: Colors.redAccent,
+//               fontSize: 50,
+//               backgroundColor: Colors.blueGrey,
+//             ),),
+//             Text("You have pushed button this meny times: "),
+//             Text("$value", style: TextStyle(
+//               fontSize: 25,
+//               color: Colors.pink,
+//               fontWeight: FontWeight.bold,
+//             ),),
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         child: Icon(Icons.add),
+//         onPressed: (){
+//           increement();
+//         },
+//       ),
+//     );
+//   }
+// }

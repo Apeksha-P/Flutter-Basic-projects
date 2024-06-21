816bfe7c39a9a4fb3e1bf29e2a87abcab5dcf8c0
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-06-22 00:28:00 +0530

    Wrap widget

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui/widgets/checkbox_widget.dart';
+import 'package:flutter_ui/widgets/wrap_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme:ThemeData(
         primarySwatch: Colors.blue,
       ) ,
-      home: const CheckBoxWidget(),
+      home: const WrapWidget(),
     );
   }
 }

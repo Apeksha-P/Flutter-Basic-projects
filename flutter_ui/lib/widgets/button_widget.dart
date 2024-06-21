7984e7f47411d4a2abeacc59f88c8bad49a2f6a1
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-06-21 20:39:47 +0530

    Create Icon BUtton

---
+++ flutter_ui/lib/widgets/button_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            IconButton(onPressed: () {
+              print("Pressed");
+            }, icon: Icon(Icons.favorite,color: Colors.redAccent,))
+          ],
+        ),
+      ),
+    );
+  }
+}

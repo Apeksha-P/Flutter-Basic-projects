eacb26bd8d85e1ea5dd1151bc4c6b478fdc4f0b7
H.P.Pavithri Apeksha <apeksha-cs20070@stu.kln.ac.lk>
2024-06-22 00:12:39 +0530

    Check box widget

---
+++ flutter_ui/lib/widgets/checkbox_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CheckBoxWidget extends StatefulWidget {
+  const CheckBoxWidget({super.key});
+
+  @override
+  State<CheckBoxWidget> createState() => _CheckBoxWidgetState();
+}
+
+class _CheckBoxWidgetState extends State<CheckBoxWidget> {
+  bool agreement = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Checkbox(value: agreement, onChanged: (val){
+          setState(() {
+            agreement = ! agreement;
+          });
+        }),
+      ),
+    );
+  }
+}
